e72ddfe861da22fac72ba9b515a041fe8ec29d05
GamerMJay <66413999+GamerMJay2008@users.noreply.github.com>
2022-01-27 01:23:26 +0100

    Create Main.php

---
+++ src/AON/allornothing/Main.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AON\allornothing;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\command\CommandSender;
+use pocketmine\command\Command;
+
+class Main extends PluginBase{
+
+    public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool
+    {
+        switch ($command->getName()) {
+            case "aon":
+                $sender->sendMessage("§8[§4AON§8] §cThe limit is:");
+                $sender->sendMessage("§8[§4AON§8] §e" . rand(500, 50000000) . "§e Money");
+                return true;
+            default:
+                return false;
+        }
+    }
+}
